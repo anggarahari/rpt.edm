85679e41f744c340db096b2a7568bb90a5777b95
hari.anggara <anggarahari95@gmail.com>
2024-12-09 12:51:23 +0700

    commit v0.1

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("smm.hcmapp.rpt.edm.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
